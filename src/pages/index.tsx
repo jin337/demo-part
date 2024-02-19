1280a215c92ddd9389c1b8595f04b41b28ee303b
jin <jin337x@outlook.com>
2024-02-19 11:12:02 +0800

    111

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ function Index() {
 
   return (
     <div>
-      {common.count}>asdassewe
+      {common.count}
       <button onClick={() => dispatch(setCount({ type: "add", count: common.count + 1 }))}>Dispatch Action</button>
     </div>
   );
