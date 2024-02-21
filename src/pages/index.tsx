940bef641633dc3cdc7bc5850656cd4ea99e778a
jin <jin337x@outlook.com>
2024-02-21 23:50:51 +0800

    feat: init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,10 @@
 import React from 'react';
 import { useAppDispatch, useAppSelector } from '@/hooks/redux'
 import { setCount } from '@/store/reducers/common';
-import Http from '@/service/api';
 
-function Index({ data }: any) {
+function Index() {
   const dispatch = useAppDispatch();
   const common = useAppSelector((state) => state.common);
-  console.log(data);
 
   return (
     <div>
@@ -15,14 +13,6 @@ function Index({ data }: any) {
       <button onClick={() => dispatch(setCount({ type: "add", count: common.count + 1 }))}>Dispatch Action</button>
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const res = await Http.get('/user')
-  const result = res.data
-  return {
-    props: { data: result }
-  }
 }
 
 export default Index
